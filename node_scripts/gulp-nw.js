05e20ed898c8b1159868e67ea037227f15b8b196
yapcheahshen <yapcheahshen@gmail.com>
2014-03-11 22:21:28 +0800

    update to nw 0.9.2

--- node_scripts/gulp-nw.js
+++ node_scripts/gulp-nw.js
@@ -1,18 +1,18 @@
 module.exports={
     'win32':{
-      url:'https://s3.amazonaws.com/node-webkit/v0.8.2/node-webkit-v0.8.2-win-ia32.zip',
+      url:'https://s3.amazonaws.com/node-webkit/v0.9.2/node-webkit-v0.9.2-win-ia32.zip',
       zip:'node_webkit/download/win-ia32.zip',
       path:'node_webkit/win-ia32',
       bin:'..\\node_webkit\\win-ia32\\nw.exe'
     },
     'darwin':{
-      url:'https://s3.amazonaws.com/node-webkit/v0.8.2/node-webkit-v0.8.2-osx-ia32.zip',
+      url:'https://s3.amazonaws.com/node-webkit/v0.9.2/node-webkit-v0.9.2-osx-ia32.zip',
       zip:'node_webkit/download/osx-ia32.zip',
       path:'node_webkit/osx-ia32',
       bin:'../node_webkit/osx-ia32/node-webkit.app/Contents/MacOS/node-webkit'
     },
     'linux':{
-      node_webkit_url:'https://s3.amazonaws.com/node-webkit/v0.8.2/node-webkit-v0.8.2-linux-ia32.zip',
+      node_webkit_url:'https://s3.amazonaws.com/node-webkit/v0.9.2/node-webkit-v0.9.2-linux-ia32.zip',
       zip:'node_webkit/download/linux-ia32.zip',
       path:'node_webkit/linux-ia32'
     }
