a3191c5e43460ea3dfa744bdc159ad97f2dddd91
yapcheahshen <yapcheahshen@gmail.com>
2014-04-05 23:05:00 +0800

    caller suppies context menu

--- components/ksanaforge-docview/index.jsx
+++ components/ksanaforge-docview/index.jsx
@@ -3,20 +3,19 @@ var kdoc=Require('ksana-document').document;
 var surface=Require("docsurface"); 
 var $=Require("jquery");
 var bootstrap=Require("bootstrap");
-var contextmenu=Require("contextmenu");
 //var othercomponent=Require("other"); 
 var docview = React.createClass({
-  getInitialState: function() {
+  getInitialState: function() { 
     var D=kdoc.createDocument();
     return {D:D, selstart:0, sellength:0, page:null};
   },
   contextMenu:function() {
-    return this.props.menu();
-  },
+    return this.props.menu({ref:"menu", onPageAction:this.onPageAction});
+  }, 
   render: function() {
     return (
       <div>
-       <contextmenu ref="menu"/>
+      {this.contextMenu()}
        <surface page={this.state.page}
                 selstart={this.state.selstart} 
                 sellength={this.state.sellength}
@@ -25,12 +24,26 @@ var docview = React.createClass({
       </div>
     );
   },
+  onPageAction:function() {
+    var args = [];
+    Array.prototype.push.apply( args, arguments );
+
+    var api=args.shift();
+    this.state.page[api].apply(this.state.page,args);
+    var newstart=this.state.selstart+this.state.sellength;
+    this.setState({selstart:newstart,sellength:0});
+  },
   onSelection:function(start,len,x,y) {
     this.setState({selstart:start,sellength:len});
-
-    $( this.refs.menu.getDOMNode() ).css({
-      left:x,top:y
-    }).addClass("open");
+    if (this.refs.menu.onPopup) {
+      var context={
+        text:this.state.page.inscription.substr(start,len),
+        selstart:start,
+        sellength:len
+      }
+      this.refs.menu.onPopup(context);
+    }
+    $(this.refs.menu.getDOMNode()).css({left:x,top:y}).addClass("open");
   },
   createPage:function() {
     this.setState({page:this.state.D.createPage(this.props.doc.text)});
