e57d7064a728087cc6b58225bf3d4af381104afa
yapcheahshen <yapcheahshen@gmail.com>
2014-04-30 17:19:57 +0800

    interface for document service

---
+++ components/ksanaforge-kse/rpc_document.js
@@ -0,0 +1,24 @@
+var rpc=require('./rpc')
+var makeinf=function(name) {
+	return (
+		function(opts,callback) {
+			rpc.exec(callback,0,"document",name,opts);
+		});
+}
+var api={};
+
+api.enumProject=makeinf("enumProject");
+api.getProjectFolders=makeinf("getProjectFolders");
+api.getProjectFiles=makeinf("getProjectFiles");
+api.openDocument=makeinf("openDocument");
+api.saveMarkup=makeinf("saveMarkup");
+api.saveDocument=makeinf("saveDocument");
+api.getUserSettings=makeinf("getUserSettings");
+
+
+rpc.exec(function(err,data){
+	api.version=data;
+},0,"document","version",{});
+
+
+module.exports=api;
